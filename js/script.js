e97d45e346bcc09ba0c9a4e2a35e65054b7c5517
Abay <ibrayev.abay@gmail.com>
2023-10-06 12:09:47 -0600

    basic index and css implementation

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* <><><><><><><>      DOM SELECTORS    <><><><><><><> */
+const canvas = document.querySelector('#canvas1');
+/* <><><><><><><>      CANVAS SETUP     <><><><><><><> */
+const ctx = canvas.getContext('2d');
+
+    // Set resolution to window
+//canvas1.setAttribute('height', getComputedStyle(canvas1).height);
+//canvas1.setAttribute('width', getComputedStyle(canvas1).width);
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx1.fillStyle = 'red';
+/* <><><><><><><>        CLASSES        <><><><><><><> */
+/* <><><><><><><>       FUNCTIONS       <><><><><><><> */
+/* <><><><><><><>    EVENT LISTENERS    <><><><><><><> */
+document.addEventListener
+
+console.log('heeyy')
