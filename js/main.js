0b1a268169528e3b70ee10435d2aa5a655193030
Abay <ibrayev.abay@gmail.com>
2023-10-08 21:27:34 -0600

    added background

--- js/main.js
+++ js/main.js
@@ -16,10 +16,7 @@ const ctx2 = canvas2.getContext('2d');
 // Glowing eyes
 const ctx3 = canvas3.getContext('2d');
 
-    // Set resolution to window
-//canvas1.setAttribute('height', getComputedStyle(canvas1).height);
-//canvas1.setAttribute('width', getComputedStyle(canvas1).width);
-
+// Best resolution 1000px
 canvas1.width = 1000;
 canvas1.height = 1000;
 canvas2.width = canvas1.width;
@@ -31,18 +28,11 @@ class Game {
     constructor(width, height) {
         this.width = width;
         this.height = height;
-        this.graceTimer = 100;
+        this.graceTimer = 10;
         this.gracePeriod = true;
         this.player = new Player(this);
         this.input = new InputHandler(this);
         this.enemy = new Demon(this);
-        // this.tree1 = new Obstacle();
-        // this.tree2 = new Obstacle();
-        // this.tree3 = new Obstacle();
-        // this.tree4 = new Obstacle();
-        // this.tree5 = new Obstacle();
-        // this.tree6 = new Obstacle();
-        //this.enemy2 = new Demon(this);
         this.trees = [];
         this.enemies = [this.enemy];
         for (let i = 0; i < 20; i++) {
@@ -50,7 +40,6 @@ class Game {
         }
         this.greenKey = new KeyObject(this);
         this.escape = new Door(this);
-        //this.trees = [this.tree1, this.tree2, this.tree3, this.tree4, this.tree5, this.tree6];
         this.keysCollected = [];
         this.playerIsAlive = true;
         this.win = false;
